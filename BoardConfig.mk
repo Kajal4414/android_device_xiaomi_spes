9a9e79d9ed3e9d60180072b7246172dac9fe4ba5
Bruno Martins <bgcngm@gmail.com>
2024-07-14 01:58:48 -0300

    spes: Inherit QTI and Xiaomi FCMs
    Change-Id: I8d4d54bba1b3dff27c11b0034852697b818ee609
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -94,8 +94,10 @@ TARGET_USES_HWC2 := true
 BOARD_HAVE_QCOM_FM := true
 
 # HIDL
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+    $(DEVICE_PATH)/configs/hidl/framework_compatibility_matrix.xml \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += k7tn
 ODM_MANIFEST_K7TN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_k7tn.xml
