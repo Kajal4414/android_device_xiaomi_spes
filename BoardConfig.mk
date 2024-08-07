b6df515100c48325834f9834dd1e05d54e85aed5
Nolen Johnson <johnsonnolen@gmail.com>
2024-08-07 13:01:44 +0530

    spes: Use legacy IMS sepolicy
    * Needed after https://review.lineageos.org/c/LineageOS/android_device_qcom_sepolicy_vndr/+/379218/.
    
    [Kajal4414] Fix legacy-um/SEPolicy path.
    
    Change-Id: I1b1acc719044d07a53db63de0466ef1309eba6e7
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -195,13 +195,14 @@ TARGET_USERIMAGES_USE_F2FS := true
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # RIL
+BOARD_USES_LEGACY_IMS_SEPOLICY := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security Patch
 VENDOR_SECURITY_PATCH := 2024-06-01
 
 # Sepolicy
-include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
+include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
