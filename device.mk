948a1d011e560f74ffd481b5f66ff0a317cfd706
Kajal4414 <81718060+Kajal4414@users.noreply.github.com>
2024-08-14 08:55:39 +0530

    spes: Build missing soundtrigger entries
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.3::ISoundTriggerHw/default in either framework or device VINTF manifest.
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.2::ISoundTriggerHw/default in either framework or device VINTF manifest.
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.1::ISoundTriggerHw/default in either framework or device VINTF manifest.
    * I hwservicemanager: getTransport: Cannot find entry android.hardware.soundtrigger@2.0::ISoundTriggerHw/default in either framework or device VINTF manifest.
    
    Change-Id: I41df539c38df5d0354df17963e56286605e0ed6c
    Signed-off-by: Kajal4414 <81718060+Kajal4414@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -93,6 +93,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
+    android.hardware.soundtrigger@2.3-impl \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl
